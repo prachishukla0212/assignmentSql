bb8c1c1042087925178d6ec84d7c0347f92aed27
prachishukla0212 <62698224+prachishukla0212@users.noreply.github.com>
2020-04-24 12:29:04 +0530

    Add files via upload

---
+++ springmvc/springmvcexample/src/main/java/com/ntt/data/service/EmployeeManager.java
@@ -0,0 +1,10 @@
+package com.ntt.data.service;
+
+import java.util.List;
+
+import com.ntt.data.model.EmployeeVO;
+
+public interface EmployeeManager 
+{
+	public List<EmployeeVO> getAllEmployees();
+}
